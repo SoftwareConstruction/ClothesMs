be181dbb49751706baebefc2310265e5fb7e756e
LintBin <312763889@qq.com>
2014-07-04 01:02:31 +0800

    优化Clothes模块的JUnit测试的代码

--- src/test/java/com/dao/impl/ClothesDAOImplTest.java
+++ src/test/java/com/dao/impl/ClothesDAOImplTest.java
@@ -1,14 +1,14 @@
 package com.dao.impl;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.util.List;
 
 import javax.annotation.Resource;
 import javax.transaction.Transactional;
 
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertThat;
-
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -26,9 +26,11 @@ public class ClothesDAOImplTest {
 	@Resource
 	private ClothesDAO clothesDAOImpl;
 	
-	@Test
-	public void save(){
-		Clothes clothes = new Clothes();
+	private Clothes clothes ;
+	private Admin admin;
+	@Before
+	public void before(){
+		clothes = new Clothes();
 		clothes.setColor("蓝色");
 		clothes.setCommodity_name("中款大衣");
 		clothes.setDocuNum("BR1703");
@@ -39,24 +41,21 @@ public class ClothesDAOImplTest {
 		clothes.setRetail_price(800);
 		clothes.setSize(150);
 		
-		Admin admin = new Admin();
-		admin.setFlag(1);
+		admin = new Admin();
 		admin.setId(1);
-		admin.setIntroduction("ja");
-		admin.setName("lin");
-		admin.setPassword("1212");
-		admin.setUsername("asd");
 		
+	}
+	@Test
+	public void save(){
 		clothes.setOperator(admin);
 		clothesDAOImpl.save(clothes);
 	}
 	
 	@Test
 	public void update(){
-		Clothes clothes = new Clothes();
-		clothes.setId(1);
+		clothes.setId(2);
 		clothes.setCommodity_name("中款大衣");
-		clothes.setDocuNum("docu1");
+		clothes.setDocuNum("docu2");
 		clothes.setEx_factory_price(450);
 		clothes.setFlag(100);
 		clothes.setIn_material("布");
@@ -64,14 +63,6 @@ public class ClothesDAOImplTest {
 		clothes.setRetail_price(800);
 		clothes.setSize(150);
 		clothes.setColor("深蓝色");
-		
-		Admin admin = new Admin();
-		admin.setId(1);
-		admin.setUsername("username1");
-		admin.setIntroduction("password1");
-		admin.setName("name1");
-		admin.setPassword("password1");
-		admin.setFlag(1);
 		
 		clothes.setOperator(admin);
 		clothesDAOImpl.update(clothes);
@@ -81,47 +72,27 @@ public class ClothesDAOImplTest {
 	
 	@Test
 	public void findClothesByDocuNum(){
-		List<Clothes> result = clothesDAOImpl.findClothesByDocuNum("docu1");
+		List<Clothes> result = clothesDAOImpl.findClothesByDocuNum("docu2");
 		assertThat(result.size(),is(1));
 	}
 	
 	@Test
 	public void findClothesPaging(){
 		List<Clothes> result = clothesDAOImpl.findAllByPaging(0, 10);
-		System.out.println(result.size());
+		assertThat(result.size(),is(10));
 	}
 	
 	@Test
 	public void updataFlag(){
-		Clothes clothes = new Clothes();
-		clothes.setId(1);
-		clothes.setCommodity_name("中款大衣");
-		clothes.setDocuNum("docu1");
-		clothes.setEx_factory_price(450);
-		clothes.setFlag(100);
-		clothes.setIn_material("布");
-		clothes.setOut_material("尼龙");
-		clothes.setRetail_price(800);
-		clothes.setSize(150);
-		clothes.setColor("深蓝色");
-		
-		Admin admin = new Admin();
-		admin.setId(1);
-		admin.setUsername("username1");
-		admin.setIntroduction("password1");
-		admin.setName("name1");
-		admin.setPassword("password1");
-		admin.setFlag(1);
-		
+		clothes.setId(2);
+		clothes.setFlag(0);
 		clothes.setOperator(admin);
 		clothesDAOImpl.updateFlag(clothes);
-		List<Clothes> result = clothesDAOImpl.findClothesByDocuNum("docu1");
-		assertThat(result.get(0).getFlag(),is(100));
 	}
 	
 	@Test
 	public void getAccount(){
 		int result = clothesDAOImpl.getAccount();
-		System.out.println(result);
+		assertThat(result,is(18));
 	}
 }
